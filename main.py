d96bf35bf443830f1ad020a7805ed8919a23b55f
Sreekar-Tammana <sreekar2122002@gmail.com>
2022-11-26 08:49:15 +0530

    image blurred and converted to gray scale

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Hello shape detector project")
+# Import Libraries
+import cv2
+
+# Setup webcam and it's height, width
+cap = cv2.VideoCapture(0)
+cap.set(3, 500)
+cap.set(4, 500)
+
+while True:
+    success, img = cap.read()
+
+    # Blur image
+    imgBlur = cv2.GaussianBlur(img, (7,7), 1)
+
+    # Display image
+    cv2.imshow("Original", img)
+
+    # Converting to GrayScale image
+    imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("Gray scale", imgGray)
+    
+    # Quit key
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
