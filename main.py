e93f06679c6464126fc66ffdb543a6653c5e3ea7
Sreekar-Tammana <sreekar2122002@gmail.com>
2022-11-26 09:10:28 +0530

    Threshold values achieved

--- main.py
+++ main.py
@@ -13,6 +13,12 @@ def empty(a):
 # Create Trackbars for handling Threshold for Canny edge detector
 cv2.namedWindow("Parameters")
 cv2.resizeWindow("Parameters", 640, 240)
+
+# Default or starting values
+# cv2.createTrackbar("Threshold1", "Parameters", 150, 255, empty)
+# cv2.createTrackbar("Threshold2", "Parameters", 255, 255, empty)
+
+# After achieving thresholds values set it to minimum values
 cv2.createTrackbar("Threshold1", "Parameters", 150, 255, empty)
 cv2.createTrackbar("Threshold2", "Parameters", 255, 255, empty)
 
@@ -28,10 +34,15 @@ while True:
 
     # Converting to GrayScale image
     imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
-    cv2.imshow("Gray scale", imgGray)
+    # cv2.imshow("Gray scale", imgGray)
+
+    # Get threshold values from trackbars
+    threshold1 = cv2.getTrackbarPos("Threshold1", "Parameters")
+    threshold2 = cv2.getTrackbarPos("Threshold2", "Parameters")
 
     # Converting imgBlur to Canny edge
-    imgCanny = cv2.Canny(imgGray, )
+    imgCanny = cv2.Canny(imgGray, threshold1, threshold2)
+    cv2.imshow("Canny image", imgCanny)
     
     # Quit key
     if cv2.waitKey(1) & 0xFF == ord('q'):
