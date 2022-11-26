3ae11c585a708eee754bb42c3f25543f43000770
Sreekar-Tammana <sreekar2122002@gmail.com>
2022-11-26 09:23:18 +0530

    contours detection added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Import Libraries
 import cv2
+import numpy as np
 
 # Setup webcam and it's height, width
 cap = cv2.VideoCapture(0)
@@ -19,12 +20,19 @@ cv2.resizeWindow("Parameters", 640, 240)
 # cv2.createTrackbar("Threshold2", "Parameters", 255, 255, empty)
 
 # After achieving thresholds values set it to minimum values
-cv2.createTrackbar("Threshold1", "Parameters", 150, 255, empty)
-cv2.createTrackbar("Threshold2", "Parameters", 255, 255, empty)
+cv2.createTrackbar("Threshold1", "Parameters", 6, 255, empty)
+cv2.createTrackbar("Threshold2", "Parameters", 163, 255, empty)
+
+# Contours Function
+def getContours(img, imgContour):
+    cnt, hie = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    cv2.drawContours(imgContour, cnt, -1, (255, 0, 255), 7)
 
 # Loop for webcam
 while True:
     success, img = cap.read()
+
+    imgContour = img.copy()
 
     # Blur image
     imgBlur = cv2.GaussianBlur(img, (7,7), 1)
@@ -43,8 +51,14 @@ while True:
     # Converting imgBlur to Canny edge
     imgCanny = cv2.Canny(imgGray, threshold1, threshold2)
     cv2.imshow("Canny image", imgCanny)
+
+    # Dilate Canny image
+    kernel = np.ones((5,5))
+    imgDil = cv2.dilate(imgCanny, kernel, iterations=1)
+    cv2.imshow("Dilate", imgDil)
+
+    getContours(imgDil, imgContour)
     
     # Quit key
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
