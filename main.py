2e846199b1a80583af50cdd66ad0d0eead757f3d
Sreekar-Tammana <sreekar2122002@gmail.com>
2022-12-03 12:26:09 +0530

    Contours found

--- main.py
+++ main.py
@@ -2,8 +2,12 @@
 import cv2
 import numpy as np
 
+# Import Image
+# img = cv2.imread('./mobile.jpg')
+
 # Setup webcam and it's height, width
-cap = cv2.VideoCapture(0)
+path = "http://192.168.0.122:8080/video"
+cap = cv2.VideoCapture(path)
 cap.set(3, 500)
 cap.set(4, 500)
 
@@ -20,13 +24,21 @@ cv2.resizeWindow("Parameters", 640, 240)
 # cv2.createTrackbar("Threshold2", "Parameters", 255, 255, empty)
 
 # After achieving thresholds values set it to minimum values
-cv2.createTrackbar("Threshold1", "Parameters", 6, 255, empty)
-cv2.createTrackbar("Threshold2", "Parameters", 163, 255, empty)
+# cv2.createTrackbar("Threshold1", "Parameters", 6, 255, empty)
+# cv2.createTrackbar("Threshold2", "Parameters", 163, 255, empty)
+
+cv2.createTrackbar("Threshold1", "Parameters", 94, 255, empty)
+cv2.createTrackbar("Threshold2", "Parameters", 57, 255, empty)
 
 # Contours Function
 def getContours(img, imgContour):
-    cnt, hie = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    cv2.drawContours(imgContour, cnt, -1, (255, 0, 255), 7)
+    contour, hie = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    for cnt in contour:
+        area = cv2.contourArea(cnt)
+        if area>10000:
+            # print(area)
+            cv2.drawContours(imgContour, cnt, -1, (255, 0, 255), 7)
 
 # Loop for webcam
 while True:
@@ -58,6 +70,7 @@ while True:
     cv2.imshow("Dilate", imgDil)
 
     getContours(imgDil, imgContour)
+    cv2.imshow("Contours", imgContour)
     
     # Quit key
     if cv2.waitKey(1) & 0xFF == ord('q'):
